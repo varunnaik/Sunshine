f0987a0d4f362c6280b5f2f4d73279b5cf00c488
Varun Naik <varun.naik@gmail.com>
2014-10-16 22:59:27 +0530

    Add setting to change units type.

--- app/src/main/java/com/example/varun/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/varun/sunshine/ForecastFragment.java
@@ -231,6 +231,16 @@ public class ForecastFragment extends Fragment {
      */
     private String formatHighLows(double high, double low) {
         // For presentation, assume the user doesn't care about tenths of a degree.
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String unitType = sharedPreferences.getString(
+                getString(R.string.pref_units_key),
+                getString(R.string.pref_units_metric)
+        );
+
+        if (unitType.equals(getString(R.string.pref_units_imperial))) {
+            high = (high * 1.8) + 32;
+            low = (low * 1.8) + 32;
+        }
         long roundedHigh = Math.round(high);
         long roundedLow = Math.round(low);
 
--- app/src/main/java/com/example/varun/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/varun/sunshine/SettingsActivity.java
@@ -21,6 +21,7 @@ public class SettingsActivity extends PreferenceActivity
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
     }
 
     /**
